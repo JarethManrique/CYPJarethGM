9e4d1818e94e3bc8d1af69d846f7ec549bece6f8
JarethManrique <yarethmanrique@gmail.com>
2019-10-08 17:40:01 -0500

    Algo

---
+++ switch.py
@@ -0,0 +1,16 @@
+dia=int(input("Introuce un dia se la semana (1-5): "))
+if dia==1 :
+    print("Lunes")
+elif dia==2 :
+    print ("Martes")
+elif dia==3 :
+    print ("Miercoles")
+elif dia==4 :
+    print ("Jueves")
+elif dia==5  :
+    print ("Viernes")
+else:
+    print("Pendejo")
+print("Fin del programa")
+
+
