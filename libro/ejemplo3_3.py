8fc339abc4ab06615163c441e99ebd0e74558a96
JarethManrique <yarethmanrique@gmail.com>
2019-10-15 17:37:32 -0500

    ejemplos

---
+++ libro/ejemplo3_3.py
@@ -0,0 +1,8 @@
+CUECER=0
+N=int(input("Dame un numero entero positivo: "))
+for i in range(0,N,1):
+    NUM=int(input("Dame un numero entero: "))
+    if NUM==0:
+        CUECER+=1
+print("El numero de 0's capturados es de:", CUECER)
+
